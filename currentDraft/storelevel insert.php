7a43192b4150c7698f37f394f90b91f90ed87b49
johnnyfco3 <72476506+johnnyfco3@users.noreply.github.com>
2020-10-28 00:43:27 +0000

    Create storelevel insert.php

---
+++ currentDraft/storelevel insert.php
@@ -0,0 +1,45 @@
+<?php
+  $con = mysqli_connect('localhost', 'root', '', 'GroceryStore');
+
+  if(!$con)
+  {
+    echo 'Not Connected To Server';
+  }
+
+  if(!mysqli_select_db($con, 'GroceryStore'))
+  {
+    echo 'Database Not Selected';
+  }
+
+  if(isset($_POST['submit']))
+  {
+    if(empty($_POST['email']) || empty($_POST['password']) || empty($_POST['fname']) || empty($_POST['lname']) || empty($_POST['number']) || empty($_POST['stores']) || empty($_POST['company']))
+    {
+      echo ' Please Fill in the Blanks ';
+    }
+    else
+    {
+      $Email = $_POST['email'];
+      $Password = $_POST['password'];
+      $FirstName = $_POST['fname'];
+      $LastName = $_POST['lname'];
+      $PhoneNumber = $_POST['number'];
+      $NumberofStores = $_POST['stores'];
+      $CompanyName = $_POST['company'];
+
+      $query = "insert into storelevel_signup (email, password, first_name, last_name, phone_number, number_of_stores, company_name)
+              values ('$Email', '$Password', '$FirstName', '$LastName', '$PhoneNumber', '$NumberofStores', '$CompanyName')";
+
+      $result = mysqli_query($con, $query);
+
+      if($result)
+      {
+        header("location:storelevelview.php");
+      }
+      else
+      {
+        echo ' Please Check Your Query ';
+      }
+    }
+  }
+?>
