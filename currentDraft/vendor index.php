b1020f65ae8a93f9bac39038b67aa95b4ae07430
johnnyfco3 <72476506+johnnyfco3@users.noreply.github.com>
2020-10-28 00:49:07 +0000

    Create vendor index.php

---
+++ currentDraft/vendor index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Vendor Document</title>
+</head>
+<body>
+  <h3> Vendor Form </h3>
+    <form action="vendorinsert.php" method="post">
+      <input type="text" placeholder=" Vendor Name " name="name">
+      <input type="text" placeholder=" Vendor Number " name="number">
+      <input type="text" placeholder=" Vendor Address " name="addy">
+      <input type="text" placeholder=" Company Code " name="code">
+      <input type="text" placeholder=" Shipments " name="ship">
+      <button name="submit">Submit</button>
+    </form>
+</body>
+</html>
