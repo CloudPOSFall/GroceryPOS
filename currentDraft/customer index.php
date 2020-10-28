36e765f468d78573bb6497557e2a03f243c108d5
johnnyfco3 <72476506+johnnyfco3@users.noreply.github.com>
2020-10-28 00:09:59 +0000

    Create customer index.php

---
+++ currentDraft/customer index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Customer Document</title>
+</head>
+<body>
+            <h3> Customer Registration Form </h3>
+            <form action="customerinsert.php" method="post">
+              <input type="text" placeholder=" Email " name="email">
+              <input type="text" placeholder=" Password " name="password">
+              <input type="text" placeholder=" First Name " name="fname">
+              <input type="text" placeholder=" Middle Initial " name="mi">
+              <input type="text" placeholder=" Last Name " name="lname">
+              <input type="text" placeholder=" Phone Number " name="number">
+              <input type="text" placeholder=" Rewards " name="rewards">
+              <button name="submit">Submit</button>
+            </form>
+</body>
+</html>
