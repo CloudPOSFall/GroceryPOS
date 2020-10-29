5032d44bed0ddd2ef1b16850c74b13af656c9fc2
johnnyfco3 <72476506+johnnyfco3@users.noreply.github.com>
2020-10-29 20:58:56 +0000

    Create storelevel edit.php

---
+++ currentDraft/storelevel edit.php
@@ -0,0 +1,40 @@
+<html>
+<head>
+  <title>Document</title>
+</head>
+<body>
+  <h3 > Update Form in PHP </h3>
+    <form action="" method="post">
+      <input type="number" placeholder=" Enter ID " name="id" step="1"/>
+      <input type="email" placeholder=" Email " name="email">
+      <input type="password" placeholder=" Password " name="password">
+      <input type="text" placeholder=" First Name " name="fname">
+      <input type="text" placeholder=" Last Name " name="lname">
+      <input type="number" placeholder=" Phone Number " name="number">
+      <input type="number" placeholder=" Number of Stores " name="stores" step="1">
+      <input type="text" placeholder=" Company Name " name="company">
+      <input type="submit" name="update" value="UPDATE"/>
+    </form>
+</body>
+</html>
+<?php
+$con = mysqli_connect("localhost", "root", "");
+$db = mysqli_select_db($con,'GroceryStore');
+
+if(isset($_POST['update']))
+{
+  $id = $_POST['id'];
+
+  $query = "update storelevel_signup set email = '$_POST[email]', password = '$_POST[password]', first_name = '$_POST[fname]', last_name = '$_POST[lname]', phone_number = '$_POST[number]', number_of_stores = '$_POST[stores]', company_name = '$_POST[company]' where ID ='$_POST[id]'";
+  $result = mysqli_query($con,$query);
+
+  if($result)
+  {
+    header("location:storelevelview.php");
+  }
+  else
+  {
+    echo ' Please Check Your Query ';
+  }
+}
+?>
