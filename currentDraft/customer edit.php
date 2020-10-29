0693a7dc3f9d4715a7f8a520250ff75bda676792
johnnyfco3 <72476506+johnnyfco3@users.noreply.github.com>
2020-10-29 20:46:37 +0000

    Create customer edit.php

---
+++ currentDraft/customer edit.php
@@ -0,0 +1,40 @@
+<html>
+<head>
+  <title>Document</title>
+</head>
+<body>
+  <h3 > Update Form in PHP </h3>
+    <form action="" method="post">
+      <input type="number" placeholder=" Enter Customer ID " name="id" step="1"/>
+      <input type="email" placeholder=" Email " name="email"/>
+      <input type="password" placeholder=" Password " name="password"/>
+      <input type="text" placeholder=" First Name " name="fname"/>
+      <input type="text" placeholder=" Middle Initial " name="mi"/>
+      <input type="text" placeholder=" Last Name " name="lname"/>
+      <input type="number" placeholder=" Phone Number " name="number"/>
+      <input type="number" placeholder=" Rewards " name="rewards" step="1"/>
+      <input type="submit" name="update" value="UPDATE"/>
+    </form>
+</body>
+</html>
+<?php
+$con = mysqli_connect("localhost", "root", "");
+$db = mysqli_select_db($con,'GroceryStore');
+
+if(isset($_POST['update']))
+{
+  $id = $_POST['id'];
+
+  $query = "update customer_info set email = '$_POST[email]', password = '$_POST[password]', first_name = '$_POST[fname]', middle_initial = '$_POST[mi]', last_name = '$_POST[lname]', phone_number = '$_POST[number]', rewards = '$_POST[rewards]' where customer_id='$_POST[id]' ";
+  $result = mysqli_query($con,$query);
+
+  if($result)
+  {
+    header("location:customerview.php");
+  }
+  else
+  {
+    echo ' Please Check Your Query ';
+  }
+}
+?>
