8b09f9a2ccffd9ad43e4cc987c802209ca6ec367
johnnyfco3 <72476506+johnnyfco3@users.noreply.github.com>
2020-10-29 18:51:14 +0000

    Create inventory sales insert.php

---
+++ currentDraft/inventory sales insert.php
@@ -0,0 +1,43 @@
+<?php
+  $con = mysqli_connect('localhost', 'root', '', 'GroceryStore');
+
+  if(!$con)
+  {
+    echo 'Not Connected To Server';
+  }
+
+  if(!mysqli_select_db($con, 'GroceryStore'))
+  {
+    echo 'Database Not Selected';
+  }
+
+  if(isset($_POST['submit']))
+  {
+    if(empty($_POST['tid']) || empty($_POST['pid']) || empty($_POST['qty']) || empty($_POST['unit']) || empty($_POST['discount']))
+    {
+      echo ' Please Fill in the Blanks ';
+    }
+    else
+    {
+      $TicketID = $_POST['tid'];
+      $ProductID = $_POST['pid'];
+      $QTY = $_POST['qty'];
+      $UnitPrice = $_POST['unit'];
+      $Discount = $_POST['discount'];
+
+      $query = "insert into inventory_sales (ticket_id, product_id, qty, unit_price, discount)
+              values ('$TicketID', '$ProductID', '$QTY', '$UnitPrice', '$Discount')";
+
+      $result = mysqli_query($con, $query);
+
+      if($result)
+      {
+        header("location:inventorysalesview.php");
+      }
+      else
+      {
+        echo ' Please Check Your Query ';
+      }
+    }
+  }
+?>
