15f1adbf33f6bf777d965f71e0234f3270095a7f
johnnyfco3 <72476506+johnnyfco3@users.noreply.github.com>
2020-10-29 18:53:22 +0000

    Create product index.php

---
+++ currentDraft/product index.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+  <title>Product Inventory</title>
+</head>
+<body>
+            <h3> Product Inventory </h3>
+            <form action="productinsert.php" method="post">
+              <input type="text" placeholder=" QR Code " name="qr">
+              <input type="text" placeholder=" Serial Number " name="serial">
+              <input type="text" placeholder=" Make " name="make">
+              <input type="text" placeholder=" Model Number " name="model">
+              <input type="text" placeholder=" Model Name " name="name">
+              <input type="text" placeholder=" Description " name="d">
+              <input type="text" placeholder=" Link of Supplier Data " name="link">
+              <input type="text" placeholder=" Cost " name="cost">
+              <input type="text" placeholder=" Selling Price " name="price">
+              <input type="text" placeholder=" MSRP " name="msrp">
+              <input type="text" placeholder=" In Stock " name="in">
+              <input type="text" placeholder=" Reorder Amount " name="amount">
+              <input type="text" placeholder=" Base Stock " name="base">
+              <input type="text" placeholder=" Vendor ID " name="vendor">
+              <button name="submit">Submit</button>
+            </form>
+</body>
+</html>
