05ecac17e22955495bcebf76481544b5f6492900
johnnyfco3 <72476506+johnnyfco3@users.noreply.github.com>
2020-10-29 18:50:29 +0000

    Create inventory sales index.php

---
+++ currentDraft/inventory sales index.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+  <title>Inventory Sales Document</title>
+</head>
+<body>
+            <h3> Inventory Sales </h3>
+            <form action="inventorysalesinsert.php" method="post">
+              <input type="text" placeholder=" Ticket ID" name="tid">
+              <input type="text" placeholder=" Product ID " name="pid">
+              <input type="text" placeholder=" QTY " name="qty">
+              <input type="text" placeholder=" Unit Price " name="unit">
+              <input type="text" placeholder=" Discount " name="discount">
+              <button name="submit">Submit</button>
+            </form>
+</body>
+</html>
