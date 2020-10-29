9562220e982166f6a1129893efacaaa3bc542b29
charles <agudelo.charles@gmail.com>
2020-10-29 18:26:50 -0400

    modifying php for inventory search

--- currentDraft/searchInventory.php
+++ currentDraft/searchInventory.php
@@ -1,25 +1,38 @@
 <?php 
     // connection to database
     include_once('config.php');
-    // connection for query
-    $con = mysqli_connect('localhost', 'root', '', 'GroceryStore');
+    session_start();
+
+    $search = "";
+
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        
+
+
+
+    }
+    
+
+
     // Run this php if Search button is pushed
     if(isset($_POST['Search'])) {
         // Grabs input from the "Search Products" text field
         $searchQuery = $_POST['searchProducts'];
         // Searches database for names like the input
-        $query = "SELECT * FROM inventory_system WHERE model_name LIKE '%searchQuery%'";
-        // Get the result from the search
-        $result = mysqli_query($con,$query);
+        $query = mysqli_query($conn, "SELECT * FROM product_inventory WHERE model_name = '$email' ");
         // Get number of matches
-        $queryResult = mysqli_num_rows($result);
+        $queryResult = mysqli_num_rows($query);
+        
+
         // Search through table for match
-        while($row = mysqli_fetch_assoc($result)){
+        /*while($row = mysqli_fetch_assoc($result)){
+            // Output amount of results
+            echo "There are " .$queryResult. " results.";
             // Output the match from the database
             echo "<div> <p>" .$row['model_name']. "</p> </div>";
-        }
+        }*/
         // Makes sure the number of matches is greater then 0
-        if($queryResult > 0) {
+        if($queryResult != 0) {
             }
          else {
             // Let us know if no matches
