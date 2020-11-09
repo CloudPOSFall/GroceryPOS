51d8ebb02b7ff9eaf82645dd5f865b0073fc2c9a
Johnny <johnnyfran20002@gmail.com>
2020-11-09 11:07:33 -0500

    combination of productForm and productview

--- currentDraft/productForm.php
+++ currentDraft/productForm.php
@@ -1,5 +1,7 @@
 <form method="get" type="button" action="<?php echo $_SERVER['PHP_SELF'];?>">
 
+    <?php include 'productview.php'; ?>
+    
     <?php include 'getProductByType.php'; ?>
 
     <input type="submit" name="submit" value="Get This Category">
