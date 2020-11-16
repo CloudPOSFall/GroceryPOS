46d50b90ae13ff0648fdea6f3123ad49a7cfb448
Johnny <johnnyfran20002@gmail.com>
2020-11-16 01:20:23 -0500

    Working template to add employee and storelevel

---
+++ currentDraft/employeesignupInsert.php
@@ -0,0 +1,34 @@
+<?php
+include_once ('config.php');
+
+  if(isset($_POST['submit']))
+  {
+      $Email = $_POST['email'];
+      $Password = $_POST['password'];
+      $FirstName = $_POST['fname'];
+      $LastName = $_POST['lname'];
+      $PhoneNumber = $_POST['phone'];
+      $SSN = $_POST['social'];
+      $StreetAddress = $_POST['street'];
+      $City = $_POST['city'];
+      $State = $_POST['state'];
+      $ZipCode = $_POST['zip'];
+
+      $query = "insert into employee_info (email, password, first_name, last_name, phone_number, SSN, street_address, city, state, zip_code)
+              values ('$Email', '$Password', '$FirstName', '$LastName', '$PhoneNumber', '$SSN', '$StreetAddress', '$City', '$State', '$ZipCode')";
+
+      $result = mysqli_query($conn, $query);
+
+      
+
+      if($result)
+      {
+        header("location:accountHomeDraft.php");
+      }
+      else
+      {
+        header("location:signupDraft.php")
+      }
+    }
+  }
+?>
