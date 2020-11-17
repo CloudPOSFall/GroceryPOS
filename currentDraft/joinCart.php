905310023cd0c6176f29466a9f77cb93b81380e1
charles <agudelo.charles@gmail.com>
2020-11-17 16:01:37 -0500

    updating join clause

--- currentDraft/joinCart.php
+++ currentDraft/joinCart.php
@@ -1,8 +1,11 @@
 <?php
 
     include_once('config.php');
-    $query = "SELECT product_inventory.productName product_inventory.cost FROM product_inventory WHERE inventory_sales.product_id = 
-            product_inventory.product_id";
+    //$query = "SELECT product_inventory.productName product_inventory.cost FROM product_inventory WHERE inventory_sales.product_id = 
+    //       product_inventory.product_id";
+
+    $query = "SELECT product_inventory.productName, product_inventory.cost, inventory_sales.qty FROM product_inventory 
+              INNER JOIN inventory_sales ON product_inventory.product_id=inventory_sales.product_id";
     $result = mysqli_query($conn, $query) or die("Execution Failed");
 
     $cartCode = "<table border='1' name='product'>";
