e996c999d73b5da133ca50098aed10aed3cfb0ed
charles <agudelo.charles@gmail.com>
2020-11-17 17:02:15 -0500

    fixed decimel format

--- currentDraft/joinCart.php
+++ currentDraft/joinCart.php
@@ -1,8 +1,6 @@
 <?php
 
     include_once('config.php');
-    //$query = "SELECT product_inventory.productName product_inventory.cost FROM product_inventory WHERE inventory_sales.product_id = 
-    //       product_inventory.product_id";
 
     $query = "SELECT product_inventory.productName, product_inventory.cost, inventory_sales.qty FROM product_inventory 
               INNER JOIN inventory_sales ON product_inventory.product_id=inventory_sales.product_id";
@@ -13,7 +11,8 @@
 
     while($row = mysqli_fetch_assoc($result)) {
         $newCost = $row['qty'] * $row['cost'];
-        $cartCode .= "<tr> <th>".$row['productName']."</th> <th>".$newCost." </th> </tr>";
+        $format = number_format($newCost, 2);
+        $cartCode .= "<tr> <th>".$row['productName']."</th> <th>".$format." </th> </tr>";
     }
 
     echo ($cartCode);
