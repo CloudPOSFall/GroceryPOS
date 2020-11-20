699407a9911a2628644e1112b2d8bc1115e47df4
charles <agudelo.charles@gmail.com>
2020-11-19 21:51:46 -0500

    switched issets

--- currentDraft/finalSale.php
+++ currentDraft/finalSale.php
@@ -5,9 +5,8 @@
 <br>
 
 <form method="get" type="button" action="<?php echo $_SERVER['PHP_SELF'];?>">
-    <input type='submit' name='finalSale' value="Complete Sale">
+    Cash Amount: <input type="text" name="cash"><input type='submit' name='change' value="Get Change">
     <?php
-
         $tax = 0;
         $discount = 0;
         $query = "SELECT tax_rate FROM tax_table WHERE TTID=1";
@@ -20,32 +19,25 @@
         $subTotal = $total;
         $total = $total + $tax;
 
-        if(isset($_GET['finalSale'])) {
-
-            $query = "UPDATE ticket_system SET quantity ='".$qtyTotal."', subtotal ='".$subTotal."', total ='".$total."', 
-                      tax ='".$tax."' WHERE sale_id = 4 ";
-            //$result = mysqli_query($conn, $query) or die("Execution Failed");
-
-        }
-
-
-    ?>
-    
-</form>
-
-<br>
-<form method="get" type="button" action="<?php echo $_SERVER['PHP_SELF'];?>">
-    Cash Amount: <input type="text" name="cash"><input type='submit' name='change' value="Get Change">
-    <?php
         if(isset($_GET['change'])) {
             $change = $_GET['cash'] - $subTotal;
             $formatted = number_format($change, 2);
     ?>
     <br>
-    <?php
-            echo('$');
+        <?php
+            echo('Change: $');
             echo($formatted);
-        }
         ?>
+    <br>
+    <input type='submit' name='finalSale' value="Complete Sale">
+    <?php
+            if(isset($_GET['finalSale'])) {
+
+                $query = "UPDATE ticket_system SET quantity ='".$qtyTotal."', subtotal ='".$subTotal."', total ='".$total."', 
+                          tax ='".$tax."' WHERE sale_id = 4 ";
+                $result = mysqli_query($conn, $query) or die("Execution Failed");
+
+            }
+        }
+    ?>
 </form>
-<br>
