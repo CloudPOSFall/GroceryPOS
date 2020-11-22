2d461abef6726a93191949a4dba701f153b5191b
charles <agudelo.charles@gmail.com>
2020-11-22 17:26:55 -0500

    testing template for cart in progress

--- currentDraft/addCart.php
+++ currentDraft/addCart.php
@@ -7,13 +7,11 @@
     //$curentId = mysqli_query($conn,$saleid);
 
     if(isset($_GET['addToCart'])) {
-        $ID = $_GET['addToCart'];
-        $query = "SELECT * FROM cart WHERE product_id LIKE $ID";
+        $query = "SELECT * FROM cart WHERE product_id = '".$_GET['addToCart']."'";
         $result = mysqli_query($conn,$query);
         $rows = mysqli_num_rows($result);
 
-        if($rows > 0)
-        {
+        if($rows > 0){
             $update = "UPDATE cart SET qty = qty + 1 WHERE product_id LIKE $ID";
             $result = mysqli_query($conn,$update);
         }else{
