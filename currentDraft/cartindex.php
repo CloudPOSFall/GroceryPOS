4228fcaad440af54b5db4f08c800ea90a76a3ad3
Johnny <johnnyfran20002@gmail.com>
2020-11-23 00:41:13 -0500

    template draft for sales report

---
+++ currentDraft/cartindex.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+  <title>Cartt Document</title>
+</head>
+<body>
+            <h3> Cart Form </h3>
+            <form action="cartinsert.php" method="post">
+              <
+              <input name="qty" placeholder="qty" ></input>
+              <input name="discount" placeholder="discount" ></input>
+              <input name="cart" type="number" placeholder="cart" ></input>
+              <input name="product" type="number" placeholder="product" ></input>
+              <button name="submit">Submit</button>
+            </form>
+</body>
+</html>
