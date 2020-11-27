0c9038661380164a8214c3d89d1a31aa3e7052b2
charles <agudelo.charles@gmail.com>
2020-11-26 19:41:41 -0500

    documentation added

--- currentDraft/getSubType.php
+++ currentDraft/getSubType.php
@@ -1,12 +1,10 @@
 <?php
     include_once('config.php');
-
+    // selects only distince sub categories from database
     $query = "SELECT DISTINCT productSubType FROM product_inventory WHERE productType = '".$productType."' ORDER BY productSubType";
-
     $result = mysqli_query($conn, $query) or die("Execution failed");
-
+    // generates select table for sub categories
     $tableCode = "<select name='productSubType'>";
-
     while($row = mysqli_fetch_assoc($result)) {
         $isSelected = "";
         if(isset($_POST['productSubType']) && $_POST['productSubType'] == $row['productSubType']) {
@@ -15,5 +13,6 @@
         $tableCode .= "<option value= ".$row['productSubType']." ".$isSelected.">".$row['productSubType']. "</option> \n ";
     }
     $tableCode .= "</select>";
+    // Displays select bar with chosen sub category
     echo($tableCode);    
 ?>
--- currentDraft/joinCart.php
+++ currentDraft/joinCart.php
@@ -2,16 +2,19 @@
 <?php
 
     include_once('config.php');
-
+    // Joins product_inventory and cart tables in order to pull names and prices
     $query = "SELECT product_inventory.productName, product_inventory.cost, cart.qty FROM product_inventory 
               INNER JOIN cart ON product_inventory.product_id=cart.product_id";
     $result = mysqli_query($conn, $query) or die("Execution Failed");
 
+    // completed cart is initialized
     $cartCode = "<table border='1' name='product'>";
     $cartCode .= "<tr> <th>Product Name</th> <th>Quantity</th> <th>Product Price</th> </tr>";
+    // initializing total and quantity
     $total = 0.00;
     $qtyTotal = 0;
 
+    // Total cost is calculated and as well as quantity
     while($row = mysqli_fetch_assoc($result)) {
         $newCost = $row['qty'] * $row['cost'];
         $format = number_format($newCost, 2);
@@ -21,6 +24,7 @@
         $newCost = 0.00;
     }
 
+    // completed cart is displayed as well as total cost
     echo($cartCode);
     echo("Total Amount: $");
     echo($total);
