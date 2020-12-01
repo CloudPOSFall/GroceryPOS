617a9f703ea5279f6b217cfd1885ce522b742a9c
charles <agudelo.charles@gmail.com>
2020-12-01 00:51:58 -0500

    working payment type

--- currentDraft/finalSale.php
+++ currentDraft/finalSale.php
@@ -8,12 +8,9 @@
 <!-- form to reload page with correct change -->
 <form method="post" type="button" action="<?php echo $_SERVER['PHP_SELF'];?>">
 
-    <!-- Option to choose a payment type -->
-    Choose a Payment Type:
-    <select name="payment">
-        <option value="Cash">Cash</option>
-        <option value="Credit">Credit</option>
-    </select> <input type="submit" name ="submit" value="Payment Type"> <br>
+
+    <?php include('payType.php') ?>
+
         <!-- Input for money recieved -->
         <br>Cash Amount: <input type="text" name="cash">
         <input type='submit' name='change' value="Get Change"/><br>
