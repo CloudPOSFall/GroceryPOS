f93ec7fb512faf50d197d210ca9b1b16ad24d21e
Alex <carlson3.alex@gmail.com>
2020-12-04 01:04:31 -0500

    updated receipts.php

--- currentDraft/receipts.php
+++ currentDraft/receipts.php
@@ -179,9 +179,11 @@ $result = mysqli_query($conn, $query);
           <div class="card" id="pagecard">
             <div class="card-body text-center">
               <span class="card-text">
-                <h5 class="card-title"><svg width="2em" height="2em" viewBox="0 0 16 16" class="bi bi-cart-plus-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                    <path fill-rule="evenodd" d="M.5 1a.5.5 0 0 0 0 1h1.11l.401 1.607 1.498 7.985A.5.5 0 0 0 4 12h1a2 2 0 1 0 0 4 2 2 0 0 0 0-4h7a2 2 0 1 0 0 4 2 2 0 0 0 0-4h1a.5.5 0 0 0 .491-.408l1.5-8A.5.5 0 0 0 14.5 3H2.89l-.405-1.621A.5.5 0 0 0 2 1H.5zM4 14a1 1 0 1 1 2 0 1 1 0 0 1-2 0zm7 0a1 1 0 1 1 2 0 1 1 0 0 1-2 0zM9 5.5a.5.5 0 0 0-1 0V7H6.5a.5.5 0 0 0 0 1H8v1.5a.5.5 0 0 0 1 0V8h1.5a.5.5 0 0 0 0-1H9V5.5z" />
-                  </svg></h5>
+                <h5 class="card-title"><svg width="2em" height="2em" viewBox="0 0 16 16" class="bi bi-printer-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                <path d="M5 1a2 2 0 0 0-2 2v1h10V3a2 2 0 0 0-2-2H5z"/>
+                <path fill-rule="evenodd" d="M11 9H5a1 1 0 0 0-1 1v3a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1v-3a1 1 0 0 0-1-1z"/>
+                <path fill-rule="evenodd" d="M0 7a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v3a2 2 0 0 1-2 2h-1v-2a2 2 0 0 0-2-2H5a2 2 0 0 0-2 2v2H2a2 2 0 0 1-2-2V7zm2.5 1a.5.5 0 1 0 0-1 .5.5 0 0 0 0 1z"/>
+                </svg></h5>
                 Print Receipt
               </span>
             </div>
@@ -314,6 +316,18 @@ $result = mysqli_query($conn, $query);
             $('.collapse.in').toggleClass('in');
             $('a[aria-expanded=true]').attr('aria-expanded', 'false');
         });
+    });
+
+        //control panel buttons hoverable shadow
+        $(document).ready(function() {
+      $(".card").hover(
+        function() {
+          $(this).addClass('shadow').css('cursor', 'pointer');
+        },
+        function() {
+          $(this).removeClass('shadow');
+        }
+      );
     });
 </script>
 <style>@media print {
