1d2c463c4a9b3aa367244d752b21e38e3a461dd4
Alex <carlson3.alex@gmail.com>
2020-12-05 19:55:47 -0500

    fixed links - forgot to repush yesterday

--- currentDraft/loginDraft.php
+++ currentDraft/loginDraft.php
@@ -4,7 +4,7 @@ include_once('config.php');
 ob_start();
 // Check if the user is already logged in, if yes then redirect him to welcome page
 if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
-  header("Location: accountHomeDraft.php");
+  header("Location: salescontrolpanel.php");
   exit;
 }
 
